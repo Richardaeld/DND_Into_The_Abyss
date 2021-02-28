642c9adb71d69fee8e9a1e283187979e270085fd
Richardaeld <richardaeld@gmail.com>
2021-02-28 21:15:58 +0000

    Initial commit

---
+++ run.py
@@ -0,0 +1,34 @@
+  
+import os
+import datetime
+from flask import (
+    Flask, flash, render_template,
+    redirect, request, session, url_for)
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
+if os.path.exists("env.py"):
+    import env
+
+
+app = Flask(__name__)
+
+
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+
+mongo = PyMongo(app)
+
+
+@app.route("/")
+def index():
+
+    return render_template('index.html')
+
+
+if __name__ == "__main__":
+    app.run(host=os.environ.get("IP"),
+            port=int(os.environ.get("PORT")),
+            debug=True)
---
+++ static/js/index.js
@@ -0,0 +1,31 @@
+
+var findDice = document.querySelectorAll(".col-4");
+findDice.forEach(rollDice);
+function rollDice(item, index){
+    item.addEventListener("click", function() {
+        let diceRoll = ((item.id).split("d"))[1]
+        diceRoll = Math.floor(Math.random()*diceRoll);
+        document.getElementsByClassName("outPut")[0].textContent = "You rolled a " + (diceRoll + 1);
+    });
+
+}
+
+var narrowMissFind = document.querySelectorAll(".narrowMissSelect");
+narrowMissFind.forEach(rollNarrowMiss)
+function rollNarrowMiss(item, index) {
+    item.addEventListener("click", function() {
+        var missType = item.id
+        var rollEvent = (Math.floor(Math.random()*50)) + 1;
+        if (missType == "melee"){
+            document.getElementById("NarrowMiss").textContent = melee[rollEvent]
+        }
+        if (missType == "range"){
+            document.getElementById("NarrowMiss").textContent = range[rollEvent]
+        }
+        if (missType == "spell"){
+            document.getElementById("NarrowMiss").textContent = spell[rollEvent]
+        }
+
+    })
+}
+
