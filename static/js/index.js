fc8b9d0769a342a6a753565a9a308fb02b38a7d3
Richardaeld <richardaeld@gmail.com>
2021-03-01 07:03:22 +0000

    Made wild magic roll functional

--- static/js/index.js
+++ static/js/index.js
@@ -1,44 +1,61 @@
+// Nav bar -- Makes content visible and invisible
 var findNav = document.querySelectorAll(".nav-icon");
 findNav.forEach(navEnable);
 function navEnable(item, index) {
     item.addEventListener("click", function() {
-        var navLoc = item.textContent.trim()
+        var navLoc = item.textContent.trim();
         if (document.getElementById(navLoc).classList.contains("make-invis") == true){
             document.getElementById(navLoc).classList.remove("make-invis");
         } else{
-            document.getElementById(navLoc).classList.add("make-invis")
+            document.getElementById(navLoc).classList.add("make-invis");
         }
     })
 }
 
-
+// Rolls a random dice by taking in the d# and rolling specific number
 var findDice = document.querySelectorAll(".dice");
 findDice.forEach(rollDice);
 function rollDice(item, index){
     item.addEventListener("click", function() {
-        let diceRoll = ((item.id).split("d"))[1]
+        let diceRoll = ((item.id).split("d"))[1];
         diceRoll = Math.floor(Math.random()*diceRoll);
         document.getElementsByClassName("outPut")[0].textContent = "You rolled a " + (diceRoll + 1);
     });
-
 }
 
+// rolls for narrow miss to publish
 var narrowMissFind = document.querySelectorAll(".narrowMissSelect");
-narrowMissFind.forEach(rollNarrowMiss)
+narrowMissFind.forEach(rollNarrowMiss);
 function rollNarrowMiss(item, index) {
     item.addEventListener("click", function() {
-        var missType = item.id
+        var missType = item.id;
         var rollEvent = (Math.floor(Math.random()*50)) + 1;
         if (missType == "melee"){
-            document.getElementById("NarrowMiss").textContent = melee[rollEvent]
+            document.getElementById("consoleOutput").textContent = melee[rollEvent];
         }
         if (missType == "range"){
-            document.getElementById("NarrowMiss").textContent = range[rollEvent]
+            document.getElementById("consoleOutput").textContent = range[rollEvent];
         }
         if (missType == "spell"){
-            document.getElementById("NarrowMiss").textContent = spell[rollEvent]
+            document.getElementById("consoleOutput").textContent = spell[rollEvent];
         }
-
     })
 }
 
+var wildMagicFind = document.querySelectorAll(".wildMagicSelect");
+wildMagicFind.forEach(rollWildMagic);
+function rollWildMagic(item) {
+        item.addEventListener("click", function() {
+
+        var magicType = item.id;
+        console.log(magicType)
+        if (magicType == "wildMagicNonRaw") {
+            let rollEvent = (Math.floor(Math.random()*100)) + 1;
+            document.getElementById("consoleOutput").textContent = wildMagicNonRaw[rollEvent]
+        }
+        if (magicType == "wildMagicRaw") {
+            let rollEvent = (Math.floor(Math.random()*50)) + 1;
+            document.getElementById("consoleOutput").textContent = wildMagicRaw[rollEvent]
+        }
+    })
+}
