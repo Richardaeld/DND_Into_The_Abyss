7e2c0c6f5f9a27623a9c49998e59040c5de6489b
Richardaeld <Richardaeld@gmail.com>
2022-04-05 13:45:51 -0400

    Updated to make better use of React

--- dm_tool/src/components/Dice.js
+++ dm_tool/src/components/Dice.js
@@ -1,14 +1,22 @@
 import React from "react"
 
 export default function Dice(props) {
+
+
+    const styles = {
+        display: props.open ? "block": "none"
+    }
+
     return (
 
-        <div 
+        <div
+            style={styles}
             className="subactiveItem col-3 dice"
             id={"rolldice" + props.name}
             onClick={props.click}
         >
             <h1>{props.name}</h1>
+
         </div>
     )
 }
