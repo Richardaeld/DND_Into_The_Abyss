4840f22911624920173b8b4ab263d4bccb10533e
Richardaeld <Richardaeld@gmail.com>
2022-04-07 12:52:10 -0400

    Created magicItemTable component object and removed original file

---
+++ dm_tool/src/components/objects/content/MagicItemTableA.js
@@ -0,0 +1,404 @@
+export default [
+    {
+        id: 1,
+        value: 'Potion of healing',
+    },
+    {
+        id: 2,
+        value: 'Potion of healing',
+    },
+    {
+        id: 3,
+        value: 'Potion of healing',
+    },
+    {
+        id: 4,
+        value: 'Potion of healing',
+    },
+    {
+        id: 5,
+        value: 'Potion of healing',
+    },
+    {
+        id: 6,
+        value: 'Potion of healing',
+    },
+    {
+        id: 7,
+        value: 'Potion of healing',
+    },
+    {
+        id: 8,
+        value: 'Potion of healing',
+    },
+    {
+        id: 9,
+        value: 'Potion of healing',
+    },
+    {
+        id: 10,
+        value: 'Potion of healing',
+    },
+    {
+        id: 11,
+        value: 'Potion of healing',
+    },
+    {
+        id: 12,
+        value: 'Potion of healing',
+    },
+    {
+        id: 13,
+        value: 'Potion of healing',
+    },
+    {
+        id: 14,
+        value: 'Potion of healing',
+    },
+    {
+        id: 15,
+        value: 'Potion of healing',
+    },
+    {
+        id: 16,
+        value: 'Potion of healing',
+    },
+    {
+        id: 17,
+        value: 'Potion of healing',
+    },
+    {
+        id: 18,
+        value: 'Potion of healing',
+    },
+    {
+        id: 19,
+        value: 'Potion of healing',
+    },
+    {
+        id: 20,
+        value: 'Potion of healing',
+    },
+    {
+        id: 21,
+        value: 'Potion of healing',
+    },
+    {
+        id: 22,
+        value: 'Potion of healing',
+    },
+    {
+        id: 23,
+        value: 'Potion of healing',
+    },
+    {
+        id: 24,
+        value: 'Potion of healing',
+    },
+    {
+        id: 25,
+        value: 'Potion of healing',
+    },
+    {
+        id: 26,
+        value: 'Potion of healing',
+    },
+    {
+        id: 27,
+        value: 'Potion of healing',
+    },
+    {
+        id: 28,
+        value: 'Potion of healing',
+    },
+    {
+        id: 29,
+        value: 'Potion of healing',
+    },
+    {
+        id: 30,
+        value: 'Potion of healing',
+    },
+    {
+        id: 31,
+        value: 'Potion of healing',
+    },
+    {
+        id: 32,
+        value: 'Potion of healing',
+    },
+    {
+        id: 33,
+        value: 'Potion of healing',
+    },
+    {
+        id: 34,
+        value: 'Potion of healing',
+    },
+    {
+        id: 35,
+        value: 'Potion of healing',
+    },
+    {
+        id: 36,
+        value: 'Potion of healing',
+    },
+    {
+        id: 37,
+        value: 'Potion of healing',
+    },
+    {
+        id: 38,
+        value: 'Potion of healing',
+    },
+    {
+        id: 39,
+        value: 'Potion of healing',
+    },
+    {
+        id: 40,
+        value: 'Potion of healing',
+    },
+    {
+        id: 41,
+        value: 'Potion of healing',
+    },
+    {
+        id: 42,
+        value: 'Potion of healing',
+    },
+    {
+        id: 43,
+        value: 'Potion of healing',
+    },
+    {
+        id: 44,
+        value: 'Potion of healing',
+    },
+    {
+        id: 45,
+        value: 'Potion of healing',
+    },
+    {
+        id: 46,
+        value: 'Potion of healing',
+    },
+    {
+        id: 47,
+        value: 'Potion of healing',
+    },
+    {
+        id: 48,
+        value: 'Potion of healing',
+    },
+    {
+        id: 49,
+        value: 'Potion of healing',
+    },
+    {
+        id: 50,
+        value: 'Potion of healing',
+    },
+    {
+        id: 51,
+        value: 'Spell scroll (cantrip)',
+    },
+    {
+        id: 52,
+        value: 'Spell scroll (cantrip)',
+    },
+    {
+        id: 53,
+        value: 'Spell scroll (cantrip)',
+    },
+    {
+        id: 54,
+        value: 'Spell scroll (cantrip)',
+    },
+    {
+        id: 55,
+        value: 'Spell scroll (cantrip)',
+    },
+    {
+        id: 56,
+        value: 'Spell scroll (cantrip)',
+    },
+    {
+        id: 57,
+        value: 'Spell scroll (cantrip)',
+    },
+    {
+        id: 58,
+        value: 'Spell scroll (cantrip)',
+    },
+    {
+        id: 59,
+        value: 'Spell scroll (cantrip)',
+    },
+    {
+        id: 60,
+        value: 'Spell scroll (cantrip)',
+    },
+    {
+        id: 61,
+        value: 'Potion of climbing',
+    },
+    {
+        id: 62,
+        value: 'Potion of climbing',
+    },
+    {
+        id: 63,
+        value: 'Potion of climbing',
+    },
+    {
+        id: 64,
+        value: 'Potion of climbing',
+    },
+    {
+        id: 65,
+        value: 'Potion of climbing',
+    },
+    {
+        id: 66,
+        value: 'Potion of climbing',
+    },
+    {
+        id: 67,
+        value: 'Potion of climbing',
+    },
+    {
+        id: 68,
+        value: 'Potion of climbing',
+    },
+    {
+        id: 69,
+        value: 'Potion of climbing',
+    },
+    {
+        id: 70,
+        value: 'Potion of climbing',
+    },
+    {
+        id: 71,
+        value: 'Spell scroll (1st level)',
+    },
+    {
+        id: 72,
+        value: 'Spell scroll (1st level)',
+    },
+    {
+        id: 73,
+        value: 'Spell scroll (1st level)',
+    },
+    {
+        id: 74,
+        value: 'Spell scroll (1st level)',
+    },
+    {
+        id: 75,
+        value: 'Spell scroll (1st level)',
+    },
+    {
+        id: 76,
+        value: 'Spell scroll (1st level)',
+    },
+    {
+        id: 77,
+        value: 'Spell scroll (1st level)',
+    },
+    {
+        id: 78,
+        value: 'Spell scroll (1st level)',
+    },
+    {
+        id: 79,
+        value: 'Spell scroll (1st level)',
+    },
+    {
+        id: 80,
+        value: 'Spell scroll (1st level)',
+    },
+    {
+        id: 81,
+        value: 'Spell scroll (1st level)',
+    },
+    {
+        id: 82,
+        value: 'Spell scroll (1st level)',
+    },
+    {
+        id: 83,
+        value: 'Spell scroll (1st level)',
+    },
+    {
+        id: 84,
+        value: 'Spell scroll (1st level)',
+    },
+    {
+        id: 85,
+        value: 'Spell scroll (1st level)',
+    },
+    {
+        id: 86,
+        value: 'Spell scroll (1st level)',
+    },
+    {
+        id: 87,
+        value: 'Spell scroll (1st level)',
+    },
+    {
+        id: 88,
+        value: 'Spell scroll (1st level)',
+    },
+    {
+        id: 89,
+        value: 'Spell scroll (1st level)',
+    },
+    {
+        id: 90,
+        value: 'Spell scroll (1st level)',
+    },
+    {
+        id: 91,
+        value: 'Spell scroll (2nd level)',
+    },
+    {
+        id: 92,
+        value: 'Spell scroll (2nd level)',
+    },
+    {
+        id: 93,
+        value: 'Spell scroll (2nd level)',
+    },
+    {
+        id: 94,
+        value: 'Spell scroll (2nd level)',
+    },
+    {
+        id: 95,
+        value: 'Potion of greater healing',
+    },
+    {
+        id: 96,
+        value: 'Potion of greater healing',
+    },
+    {
+        id: 97,
+        value: 'Potion of greater healing',
+    },
+    {
+        id: 98,
+        value: 'Potion of greater healing',
+    },
+    {
+        id: 99,
+        value: 'Bag of holding',
+    },
+    {
+        id: 100,
+        value: 'Driftglobe'
+    },
+
+
+]
