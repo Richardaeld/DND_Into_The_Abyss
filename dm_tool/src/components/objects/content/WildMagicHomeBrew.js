cd6a9de617fde36472ba2c3e05aae7798f657ff2
Richardaeld <Richardaeld@gmail.com>
2022-04-07 12:48:54 -0400

    Created wildmagic component objects and removed original file

---
+++ dm_tool/src/components/objects/content/WildMagicHomeBrew.js
@@ -0,0 +1,404 @@
+export default [
+    {
+        id: 1,
+        value: "Wild Magic Mishap",
+    },
+    {
+        id: 2,
+        value: "Reduced Potency: Disadvantage on Damage Die",
+    },
+    {
+        id: 3,
+        value: "Flared: Spell is halved between target and random adjacent target/tile",
+    },
+    {
+        id: 4,
+        value: "Hatred: Disadvantage to all attacks not this target until it's dead",
+    },
+    {
+        id: 5,
+        value: "Sprain : -5ft movement until short rest",
+    },
+    {
+        id: 6,
+        value: "Puzzled: DC 12 INT (Action) to cast again",
+    },
+    {
+        id: 7,
+        value: "Touching the abyss: Gain one point of madness DC 12 Wis/Cha Save",
+    },
+    {
+        id: 8,
+        value: "Sizzlin: you and target DC 12 WIS or lose next action",
+    },
+    {
+        id: 9,
+        value: "",
+    },
+    {
+        id: 10,
+        value: "Dust in eye:DC 15 DEX/CON or -2 to your next atk",
+    },
+    {
+        id: 11,
+        value: "Loss of awareness: -1 AC, attacks have adv next round",
+    },
+    {
+        id: 12,
+        value: "Short Leave: Lose concentration for 1 round, -2 to saves",
+    },
+    {
+        id: 13,
+        value: "Lost Confidence: Disadvantage vs target until dead or short rest",
+    },
+    {
+        id: 14,
+        value: "Wicked Headache: DC 15 CON -1 Int until short rest",
+    },
+    {
+        id: 15,
+        value: "Arcane Reverb:DC 15 spell mod or 1/4 dmg to caster",
+    },
+    {
+        id: 16,
+        value: "Failed Dodge: DC 15 STR/CON orTarget Moves 5-15ft and provokes oppertunity attk",
+    },
+    {
+        id: 17,
+        value: "Miss step Sprain: DC 15 DEX/CON or -1/2 movement until short rest +heal",
+    },
+    {
+        id: 18,
+        value: "White Knuckle:Dbl Mod DMG, DC 15 Con or Disadv on dmg dice for + 1d2 rnds",
+    },
+    {
+        id: 19,
+        value: "Casters block: DC 15 spell mod or Can't cast magic next round",
+    },
+    {
+        id: 20,
+        value: "Leg Cramp: Lose all movement action next round",
+    },
+    {
+        id: 21,
+        value: "Hand cramp: cannot use S,V for 1d2 round(s)",
+    },
+    {
+        id: 22,
+        value: "Unflinching Enemy: DC 14 WIS Saving throw or become frightened",
+    },
+    {
+        id: 23,
+        value: "Sign of weakness: First attack against player gains advantage on next turn",
+    },
+    {
+        id: 24,
+        value: "Sign of weakness: First attack against player gains advantage on next turn",
+    },
+    {
+        id: 25,
+        value: "Stumble: DC 15 DEX or fall prone",
+    },
+    {
+        id: 26,
+        value: "Stumble: DC 15 DEX or fall prone",
+    },
+    {
+        id: 27,
+        value: "Arcane Consumption: 20 DC spell mod, targe takes MAX dmg or attker is stunned 1 rnd",
+    },
+    {
+        id: 28,
+        value: "Arcane Consumption: 20 DC spell mod, targe takes MAX dmg or attker is stunned 1 rnd",
+    },
+    {
+        id: 29,
+        value: "Arcane Arc: DC 20 caster mod or DEX or Take 1d4+1 force dmg + blown 10 feet back",
+    },
+    {
+        id: 30,
+        value: "Arcane Arc: DC 20 caster mod or DEX or Take 1d4+1 force dmg + blown 10 feet back",
+    },
+    {
+        id: 31,
+        value: "Arcane Arc: DC 20 caster mod or DEX or Take 1d4+1 force dmg + blown 10 feet back",
+    },
+    {
+        id: 32,
+        value: "Aether Crackback: DC 15 spell mod or 1/4 Spell DMG for caster",
+    },
+    {
+        id: 33,
+        value: "Aether Crackback: DC 15 spell mod or 1/4 Spell DMG for caster",
+    },
+    {
+        id: 34,
+        value: "Aether Crackback: DC 15 spell mod or 1/4 Spell DMG for caster",
+    },
+    {
+        id: 35,
+        value: "Aether Crackback: DC 15 spell mod or 1/4 Spell DMG for caster",
+    },
+    {
+        id: 36,
+        value: "Additional Target w/ in proxy",
+    },
+    {
+        id: 37,
+        value: "Additional Target w/ in proxy",
+    },
+    {
+        id: 38,
+        value: "Additional Target w/ in proxy",
+    },
+    {
+        id: 39,
+        value: "Additional Target w/ in proxy",
+    },
+    {
+        id: 40,
+        value: "Shell shocked: Max DMG DC 20 caster mod or become blinded d2 rounds",
+    },
+    {
+        id: 41,
+        value: "Shell shocked: Max DMG DC 20 caster mod or become blinded d2 rounds",
+    },
+    {
+        id: 42,
+        value: "Shell shocked: Max DMG DC 20 caster mod or become blinded d2 rounds",
+    },
+    {
+        id: 43,
+        value: "Shell shocked: Max DMG DC 20 caster mod or become blinded d2 rounds",
+    },
+    {
+        id: 44,
+        value: "Bad Footing: DC 15 DEX or attks have advantage next round",
+    },
+    {
+        id: 45,
+        value: "Bad Footing: DC 15 DEX or attks have advantage next round",
+    },
+    {
+        id: 46,
+        value: "Bad Footing: DC 15 DEX or attks have advantage next round",
+    },
+    {
+        id: 47,
+        value: "Bad Footing: DC 15 DEX or attks have advantage next round",
+    },
+    {
+        id: 48,
+        value: "Bad Footing: DC 15 DEX or attks have advantage next round",
+    },
+    {
+        id: 49,
+        value: "Bad Footing: DC 15 DEX or attks have advantage next round",
+    },
+    {
+        id: 50,
+        value: "Extreme Arcane Exertion: Critical hit to target, attacker left winded(stunned) next round"
+    },
+    {
+        id: 51,
+        value: 'Spectral shield, +2 AC, no magic missile for 1 minute',
+    },
+    {
+        id: 52,
+        value: 'Caster gains 3’ prehensile tail for 1 hour',
+    },
+    {
+        id: 53,
+        value: 'Cannot get drunk for 5d6 days',
+    },
+    {
+        id: 54,
+        value: 'All unlocked doors/windows in 60’ fly open',
+    },
+    {
+        id: 55,
+        value: 'Hair falls out, grows back within 24 hours',
+    },
+    {
+        id: 56,
+        value: 'Caster’s face blackened by small explosion',
+    },
+    {
+        id: 57,
+        value: 'For 1 minute, flammable touch (not worn/carried)',
+    },
+    {
+        id: 58,
+        value: 'Geyser lifts caster 50’ in air until start of next turn',
+    },
+    {
+        id: 59,
+        value: 'Regain lowest-level expended spell slot',
+    },
+    {
+        id: 60,
+        value: 'A confused bear appears within 60’ for 1 minute',
+    },
+    {
+        id: 61,
+        value: 'For 1 minute, can only shout when speaking',
+    },
+    {
+        id: 62,
+        value: 'Grass instantly sprouts to 3’ tall within 20’, 1 minute',
+    },
+    {
+        id: 63,
+        value: 'Cast fog cloud centered on self',
+    },
+    {
+        id: 64,
+        value: 'Caster can only breathe underwater for next minute',
+    },
+    {
+        id: 65,
+        value: 'Up to 3 creatures of choice in 30’:4d10 lightning dmg',
+    },
+    {
+        id: 66,
+        value: 'Caster falls Unconscious until start of next turn',
+    },
+    {
+        id: 67,
+        value: 'Frightened by nearest creature until end of next turn',
+    },
+    {
+        id: 68,
+        value: 'If target dies in next minute, its ghost haunts caster',
+    },
+    {
+        id: 69,
+        value: 'Everyone in 30’: invisible for 1 minute or till atk/cast',
+    },
+    {
+        id: 70,
+        value: 'All within 60’ saves vs Wis 15 or drops w/e they hold',
+    },
+    {
+        id: 71,
+        value: 'Resistance to all damage for next minute',
+    },
+    {
+        id: 72,
+        value: 'Everything within 20’ pulled 10’ toward caster',
+    },
+    {
+        id: 73,
+        value: 'Random creature within 60’ is poisoned for 24 hours',
+    },
+    {
+        id: 74,
+        value: 'Caster smells like lavender for 1d6 days',
+    },
+    {
+        id: 75,
+        value: 'Glow brightly for 1 minute. Blind others within 5’.',
+    },
+    {
+        id: 76,
+        value: 'Casters clothes become uncomfortably tight',
+    },
+    {
+        id: 77,
+        value: 'Cast polymorph on self. Fail save: Sheep form.',
+    },
+    {
+        id: 78,
+        value: 'If spell would kill target, target’s extremities fly apart',
+    },
+    {
+        id: 79,
+        value: 'Illusory butterflies/flowers flutter in 5’ radius 1 min.',
+    },
+    {
+        id: 80,
+        value: 'Caster trapped in a giant glass ball',
+    },
+    {
+        id: 81,
+        value: 'Take one additional action immediately',
+    },
+    {
+        id: 82,
+        value: 'Caster’s fists become huge, deal 1d8 B dmg, 1 min.',
+    },
+    {
+        id: 83,
+        value: 'Others in 30’: 1d10 necro dmg. Gain HP equal to loss',
+    },
+    {
+        id: 84,
+        value: 'Loud voice ridicules caster on init count 20 for 1 min',
+    },
+    {
+        id: 85,
+        value: 'Cast mirror image',
+    },
+    {
+        id: 86,
+        value: 'Caster’s arms become tentacles for 1 minute',
+    },
+    {
+        id: 87,
+        value: 'Cast fly on random creature within 60’',
+    },
+    {
+        id: 88,
+        value: 'Large floating eye follows caster for 1 hour',
+    },
+    {
+        id: 89,
+        value: 'Become invisible/silent for 1 minute or till atk/cast',
+    },
+    {
+        id: 90,
+        value: 'Caster’s INT and STR swap for 1 hour',
+    },
+    {
+        id: 91,
+        value: 'If you die in next minute, come back per reincarnate',
+    },
+    {
+        id: 92,
+        value: 'Caster sees everyone as a decaying corpse for 24h',
+    },
+    {
+        id: 93,
+        value: 'Size increases by 1 size category for 1 minute',
+    },
+    {
+        id: 94,
+        value: 'All light sources within 60’ radius extinguished',
+    },
+    {
+        id: 95,
+        value: 'You and all in 30’: vulnerable to piercing dmg, 1 min.',
+    },
+    {
+        id: 96,
+        value: 'Caster becomes frightened of a color for 1 hour',
+    },
+    {
+        id: 97,
+        value: 'Surrounded by faint ethereal music for 1 minute',
+    },
+    {
+        id: 98,
+        value: 'Caster suffers a head cold for 24 hours',
+    },
+    {
+        id: 99,
+        value: 'Regain all expended Sorcery Points',
+    },
+    {
+        id: 100,
+        value: 'Next phrase spoken by caster becomes true',
+    },
+
+
+]
---
+++ dm_tool/src/components/objects/content/WildMagicRaw.js
@@ -0,0 +1,202 @@
+export default [
+    {
+        id: 1,
+        value: 'Roll on this table at the start of each of your turns for the next minute, ignoring this result on subsequent rolls.',
+    },
+    {
+        id: 2,
+        value: 'For the next minute, you can see any invisible creature if you have line of sight to it.',
+    },
+    {
+        id: 3,
+        value: 'A modron chosen and controlled by the DM appears in an unoccupied space within 5 feet of you, then disappears I minute later.',
+    },
+    {
+        id: 4,
+        value: 'You cast Fireball as a 3rd-level spell centered on yourself.',
+    },
+    {
+        id: 5,
+        value: 'You cast Magic Missile as a 5th-level spell.',
+    },
+    {
+        id: 6,
+        value: 'Roll a d10. Your height changes by a number of inches equal to the roll. If the roll is odd, you shrink. If the roll is even, you grow.',
+    },
+    {
+        id: 7,
+        value: 'You cast Confusion centered on yourself.',
+    },
+    {
+        id: 8,
+        value: 'For the next minute, you regain 5 hit points at the start of each of your turns.',
+    },
+    {
+        id: 9,
+        value: 'You grow a long beard made of feathers that remains until you sneeze, at which point the feathers explode out from your face.',
+    },
+    {
+        id: 10,
+        value: 'You cast Grease centered on yourself.',
+    },
+    {
+        id: 11,
+        value: 'Creatures have disadvantage on saving throws against the next spell you cast in the next minute that involves a saving throw.',
+    },
+    {
+        id: 12,
+        value: 'Your skin turns a vibrant shade of blue. A Remove Curse spell can end this effect.',
+    },
+    {
+        id: 13,
+        value: 'An eye appears on your forehead for the next minute. During that time, you have advantage on Wisdom (Perception) checks that rely on sight.',
+    },
+    {
+        id: 14,
+        value: 'For the next minute, all your spells with a casting time of 1 action have a casting time of 1 bonus action.',
+    },
+    {
+        id: 15,
+        value: 'You teleport up to 60 feet to an unoccupied space of your choice that you can see.	',
+    },
+    {
+        id: 16,
+        value: 'You are transported to the Astral Plane until the end of your next turn, after which time you return to the space you previously occupied or the nearest unoccupied space if that space is occupied.',
+    },
+    {
+        id: 17,
+        value: 'Maximize the damage of the next damaging spell you cast within the next minute.',
+    },
+    {
+        id: 18,
+        value: 'Roll a d10. Your age changes by a number of years equal to the roll. If the roll is odd, you get younger (minimum 1 year old). If the roll is even, you get older.',
+    },
+    {
+        id: 19,
+        value: '1d6 flumphs controlled by the DM appear in unoccupied spaces within 60 feet of you and are frightened of you. They vanish after 1 minute.',
+    },
+    {
+        id: 20,
+        value: 'You regain 2d10 hit points.',
+    },
+    {
+        id: 21,
+        value: 'You turn into a potted plant until the start of your next turn. While a plant, you are incapacitated and have vulnerability to all damage. If you drop to 0 hit points, your pot breaks, and your form reverts.',
+    },
+    {
+        id: 22,
+        value: 'For the next minute, you can teleport up to 20 feet as a bonus action on each of your turns.',
+    },
+    {
+        id: 23,
+        value: 'You cast Levitate on yourself.',
+    },
+    {
+        id: 24,
+        value: 'A unicorn controlled by the DM appears in a space within 5 feet of you, then disappears 1 minute later.',
+    },
+    {
+        id: 25,
+        value: 'You cant speak for the next minute. Whenever you try, pink bubbles float out of your mouth.	',
+    },
+    {
+        id: 26,
+        value: 'A spectral shield hovers near you for the next minute, granting you a +2 bonus to AC and immunity to Magic Missile.',
+    },
+    {
+        id: 27,
+        value: 'You regain all expended sorcery points.',
+    },
+    {
+        id: 28,
+        value: 'You are immune to being intoxicated by alcohol for the next 5d6 days.',
+    },
+    {
+        id: 29,
+        value: 'Your hair falls out but grows back within 24 hours.',
+    },
+    {
+        id: 30,
+        value: 'For the next minute, any flammable object you touch that isnt being worn or carried by another creature bursts into flame.',
+    },
+    {
+        id: 31,
+        value: 'You regain your lowest-level expended spell slot.',
+    },
+    {
+        id: 32,
+        value: 'For the next minute, you must shout when you speak.',
+    },
+    {
+        id: 33,
+        value: 'You cast Fog Cloud centered on yourself.',
+    },
+    {
+        id: 34,
+        value: 'Up to three creatures you choose within 30 feet of you take 4d10 lightning damage.',
+    },
+    {
+        id: 35,
+        value: 'You are frightened by the nearest creature until the end of your next turn.',
+    },
+    {
+        id: 36,
+        value: 'Each creature within 30 feet of you becomes invisible for the next minute. The invisibility ends on a creature when it attacks or casts a spell.',
+    },
+    {
+        id: 37,
+        value: 'You gain resistance to all damage for the next minute.',
+    },
+    {
+        id: 38,
+        value: 'A random creature within 60 feet of you becomes poisoned for 1d4 hours.',
+    },
+    {
+        id: 39,
+        value: 'You glow with bright light in a 30-foot radius for the next minute. Any creature that ends its turn within 5 feet of you is blinded until the end of its next turn.',
+    },
+    {
+        id: 40,
+        value: 'You cast Polymorph on yourself. If you fail the saving throw, you turn into a sheep for the spells duration.',
+    },
+    {
+        id: 41,
+        value: 'Illusory butterflies and flower petals flutter in the air within 10 feet of you for the next minute.',
+    },
+    {
+        id: 42,
+        value: 'You can take one additional action immediately.',
+    },
+    {
+        id: 43,
+        value: 'Each creature within 30 feet of you takes 1d10 necrotic damage. You regain hit points equal to the sum of the necrotic damage dealt.',
+    },
+    {
+        id: 44,
+        value: 'You cast Mirror Image.',
+    },
+    {
+        id: 45,
+        value: 'You cast Fly on a random creature within 60 feet of you.',
+    },
+    {
+        id: 46,
+        value: 'You become invisible for the next minute. During that time, other creatures cant hear you. The invisibility ends if you attack or cast a spell.',
+    },
+    {
+        id: 47,
+        value: 'If you die within the next minute, you immediately come back to life as if by the Reincarnate spell.',
+    },
+    {
+        id: 48,
+        value: 'Your size increases by one size category for the next minute.',
+    },
+    {
+        id: 49,
+        value: 'You and all creatures within 30 feet of you gain vulnerability to piercing damage for the next minute.',
+    },
+    {
+        id: 50,
+        value: 'You are surrounded by faint, ethereal music for the next minute.'
+    },
+]
