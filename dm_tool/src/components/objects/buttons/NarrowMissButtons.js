d2894ba2f478b43135abdc27b5302b67521fc3ef
Richardaeld <Richardaeld@gmail.com>
2022-04-07 15:44:14 -0400

    Updated obj for experiment with dynamically inserting objects

--- dm_tool/src/components/objects/buttons/NarrowMissButtons.js
+++ dm_tool/src/components/objects/buttons/NarrowMissButtons.js
@@ -1,17 +1,22 @@
+import NarrowMissMelee from "../content/NarrowMissMelee";
+
 export default [
     {
         id: 1,
         name: "Melee",
+        objName: "NarrowMissMelee",
         open: false
     },
     {
         id: 1,
         name: "Ranged",
+        objName: "narrowSuccessRanged",
         open: false
     },
     {
         id: 1,
         name: "Magic",
+        objName: "NarrowMissMagic",
         open: false
     },
 ]
