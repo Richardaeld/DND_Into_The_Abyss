424fdb7def65d44c044b6ee87c33ba23007ebc86
Richardaeld <Richardaeld@gmail.com>
2022-04-08 16:35:40 -0400

    Added a new class for selected nav buttons

--- src/components/html/buttons/NavButton.js
+++ src/components/html/buttons/NavButton.js
@@ -4,18 +4,28 @@ export default function NavButton(props) {
 
     const styles = {
         backgroundColor: props.open ? "#faf9f6" : "#010203",
-        color: props.open ? "#010203" : "#faf9f6"
+        color: props.open ? "#010203" : "#faf9f6",
     }
 
     return (
+        <>
+            {!props.open && <div
+                className="nav-button custom-button"
+                style={styles}
+                onClick={props.toggleNav}
+            >
+                <p>{props.name}</p>
+            </div>}
 
-        <div
-            className="nav-button custom-button"
-            style={styles}
-            onClick={props.toggleNav}
-        >
-            <p>{props.name}</p>
-        </div>
+            {props.open && <div
+                className="nav-button custom-button deep-shadow"
+                style={styles}
+                onClick={props.toggleNav}
+            >
+                <p>{props.name}</p>
+            </div>}
+
+        </>
     )
 }
 
