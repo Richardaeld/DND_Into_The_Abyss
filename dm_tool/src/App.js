806db56da8712b2a87b3471c59e95ad972930d24
Richardaeld <Richardaeld@gmail.com>
2022-04-07 21:52:52 -0400

    Added function to magic spell nav button and all its sub buttons

--- dm_tool/src/App.js
+++ dm_tool/src/App.js
@@ -9,6 +9,9 @@ import NarrowMissButtons from './components/objects/buttons/NarrowMissButtons';
 import SuccessButton from './components/buttons/SuccessButton'
 import WildMagicButtons from './components/objects/buttons/WildMagicButtons';
 import MagicItemButtons from './components/objects/buttons/MagicItemButtons';
+import SpellButtons from './components/objects/buttons/SpellButtons';
+// import SpellInfo from './components/objects/buttons/SpellButtons'
+import SpellButton from './components/buttons/SpellButton'
 
 // content objects
 import NavButtonNames from "./components/objects/buttons/NavButtonNames";
@@ -19,6 +22,7 @@ import NarrowMissMagic from './components/objects/content/NarrowMissMagic';
 import WildMagicRaw from './components/objects/content/WildMagicRaw';
 import WildMagicHomeBrew from './components/objects/content/WildMagicHomeBrew';
 import MagicItemTableA from './components/objects/content/MagicItemTableA';
+import AllSpells from './components/json/AllSpells'
 
 // output object
 import OutContainer from './components/OutContainer';
@@ -31,6 +35,9 @@ function App() {
   const [narrowSuccessButtons, setNarrowSuccessButtons] = React.useState(NarrowMissButtons)
   const [wildMagicButtons, setWildMagicButtons] = React.useState(WildMagicButtons)
   const [magicItemButtons, setMagicItemButtons] = React.useState(MagicItemButtons)
+  const [spellButtons, setSpellButtons] = React.useState(SpellButtons)
+  const [spellLevelButtons, setSpellLevelButtons] = React.useState([])
+  const [spellInfo, setSpellInfo] = React.useState([])
   // content obj
   const [narrowSuccessMelee, setNarrowSuccessMelee] = React.useState(NarrowMissMelee)
   const [narrowSuccessRanged, setNarrowSuccessRanged] = React.useState(NarrowMissRanged)
@@ -38,6 +45,7 @@ function App() {
   const [wildMagicHomeBrew, setWildMagicHomeBrew] = React.useState(WildMagicHomeBrew)
   const [wildMagicRaw, setWildMagicRaw] = React.useState(WildMagicRaw)
   const [magicItemTableA, setMagicItemTableA] = React.useState(MagicItemTableA)
+  const [allSpells, setAllSpells] = React.useState(AllSpells)
   // return output obj
   const [outTarget, setOutTarget] = React.useState([,"Welcome to DM Tool a friendly quick reference for DMs and Players alike!"])
 
@@ -48,6 +56,9 @@ function App() {
     const diceRoll = Math.floor(Math.random()*num + 1)
     setOutTarget([diceRoll])
   }
+
+// console.log(allSpells["spells"][0]["name"])
+
 
 // run success logic
   function successLogic(id, name, obj) {
@@ -77,6 +88,9 @@ function App() {
     } else if (name == "tableA") {
       diceRoll = Math.floor(Math.random()*magicItemTableA.length + 1)
       text = magicItemTableA[diceRoll].value
+
+    } else if (name == "spells") {
+      console.log("Not built yet!!")
     }
 
 
@@ -84,6 +98,28 @@ function App() {
     console.log(outTarget)
 
   }
+
+
+// open spell name buttons
+function spellLogic(id, name, value) {
+  // console.log(id, name, value)
+  var output = []
+  allSpells["spells"].map(x => {
+    if (x["level"] == value){
+      return output.push(x)
+    }
+  })
+  console.log(output)
+  setSpellLevelButtons(output)
+}
+
+function spellDisplay(name, obj) {
+  let spellDescriptPosition = document.getElementById("spell-description")
+  let spellDescriptPositionY = spellDescriptPosition.getBoundingClientRect()
+  window.scrollTo(0, spellDescriptPositionY.y + window.pageYOffset - 15)
+  setSpellInfo([obj])
+}
+
 
   // Toggle visibility of sub nav buttons
   function toggleNav(id, targetId) {
@@ -111,6 +147,14 @@ function App() {
         })
       })
     }
+    if(!(targetId == "spells")){
+      setSpellButtons(prev => {
+        return prev.map((prevState) => {
+          return {...prevState, open: false}
+        })
+      })
+    }
+
 
     // Reveal navigation sub buttons
     if (targetId == "dice"){
@@ -134,6 +178,12 @@ function App() {
       })
     } else if (targetId == "items") {
       setMagicItemButtons(prev => {
+        return prev.map((prevState) => {
+          return {...prevState, open: !prevState.open}
+        })
+      })
+    } else if (targetId == "spells") {
+      setSpellButtons(prev => {
         return prev.map((prevState) => {
           return {...prevState, open: !prevState.open}
         })
@@ -197,6 +247,43 @@ function App() {
     />
   ))
 
+  const spellElements = spellButtons.map ((button) => (
+    <SuccessButton
+      key={button.id}
+      name={button.name}
+      open={button.open}
+      value={button.value}
+      click={() => spellLogic(button.id, button.name, button.value)}
+    />
+  ))
+  
+  const spellLevelElements = spellLevelButtons.map ((button) => (
+    <SuccessButton
+      key={button.name}
+      name={button.name}
+      open={true}
+      value={button.value}
+      click={() => spellDisplay(button.name, button)}
+      />
+  ))
+
+const spellInfoElement = spellInfo.map((info) => (
+  <SpellButton
+    key={info.name}
+    name={info.name}
+    castingTime={info.casting_time}
+    range={info.range}
+    duration={info.duration}
+    type={info.type}
+    ritual={info.ritual}
+    school={info.school}
+    components={info.components.raw}
+    description={info.description}
+    classes={info.classes}
+    tags={info.tags}
+  />
+))
+
   // ------------------------------------DOM return
   return (
     <main>
@@ -207,19 +294,23 @@ function App() {
                 {navElements}
           </div>
           <div className="col-9 mt-5">
-            {/* <div className="row justify-content-center">
-              <div className="col-lg-4 col-md-6"> */}
-                {navDiceElements}
-                {narrowMissElements}
-                {wildMagicElements}
-                {magicItemElements}
-              {/* </div>
-            </div> */}
+            {navDiceElements}
+            {narrowMissElements}
+            {wildMagicElements}
+            {magicItemElements}
+            {spellElements}
           </div>
         </div>
       </header>
 
-      {/* Dice Section */}
+      <section id="spells">
+        {spellLevelElements}
+      </section>
+
+      <section className="container-fluid spell-display mt-5" id="spell-description">
+            {spellInfoElement}
+      </section>
+
       <section className="container-fluid">
           <OutContainer
             num = {outTarget[0]}
