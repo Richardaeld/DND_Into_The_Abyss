a0ec62c4136b6f3ee45708e6760922dfc35592ef
Richardaeld <Richardaeld@gmail.com>
2022-04-09 20:54:58 -0400

    Updated UX of general output

--- src/components/html/OutContainer.js
+++ src/components/html/OutContainer.js
@@ -7,8 +7,8 @@ export default function OutContainer (props) {
             <div className="col-12">
                 <div className="row justify-content-center">
                     <div className="col-6 output index-background">
-                        {props.num && <p id="consoleOutput">You Rolled a <strong>{props.num}</strong>!</p>}
-                        {!(props.content == "") && <p id="consoleOutput">{props.content}</p>}
+                        {props.num && <p className="spell-info-p" id="consoleOutput">You Rolled a: <strong>{props.num}</strong></p>}
+                        {!(props.content == "") && <p className="spell-info-p" id="consoleOutput"><strong>{props.content}</strong></p>}
                     </div>
                 </div>
             </div>
