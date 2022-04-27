4783bc3dfae1f909e3456ae0b3d23a15613fb79a
Richardaeld <Richardaeld@gmail.com>
2022-04-27 00:37:42 -0400

    updated to always display for development needs

--- src/components/html/buttons/SuccessNewButton.js
+++ src/components/html/buttons/SuccessNewButton.js
@@ -7,13 +7,16 @@ export default function SuccessButton(props) {
         display: true ? "inline-block" : "none"
     }
 
+    // console.log("I AM PROPS", props.obj_name)
+
     return (
         <div
             style={styles}
             className="nonselectable hand-drawn-text hand-drawn-container hand-drawn-border"
-            // onClick={props.click}
+            onClick={props.click}
         >
-            <p><strong>{props.name}</strong></p>
+            {/* <p><strong>{props.name}</strong></p> */}
+            <p><strong>{props.obj_name ? props.obj_name : props.name}</strong></p>
 
         </div>
     )
