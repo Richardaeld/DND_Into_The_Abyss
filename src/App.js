6f23e5d31e97d78f9df22d9d8df7bb8e1baed983
Richardaeld <Richardaeld@gmail.com>
2022-04-30 14:23:12 -0400

    Added all new info to card

--- src/App.js
+++ src/App.js
@@ -339,15 +339,22 @@ const spellInfoElement = spellInfo.map((info) => (
   <SpellButton
     key={info.name}
     name={info.name}
+    level={info.level}
     castingTime={info.casting_time}
+    reaction_condition={info.reaction_condition}
     range={info.range}
     duration={info.duration}
     // type={info.type}
     ritual={info.ritual}
+    component_somatic={info.component_somatic}
+    component_verbal={info.component_verbal}
+    component_material={info.component_material}
+    material_description={info.material_description}
     school={info.school}
     // components={info.components.raw}
     description={info.description}
     classes={info.classes}
+    higher_levels={info.higher_levels}
     // tags={info.tags}
     // toTop={toTop}
     open={true}
--- src/components/html/buttons/SpellButton.js
+++ src/components/html/buttons/SpellButton.js
@@ -13,8 +13,17 @@ export default function Spells(props) {
                                 <p><strong>{props.name}</strong></p>
                             </div>
                             <div className="col-6 col-md-3">
+                                <p className="mb-0 spell-key">Level</p>
+                                {props.level === 0 &&<p><strong>Cantrip</strong></p>}
+                                {props.level !== 0 &&<p><strong>Level {props.level}</strong></p>}
+                            </div>
+                            <div className="col-6 col-md-3">
                                 <p className="mb-0 spell-key">Casting Time</p>
                                 <p><strong>{props.castingTime}</strong></p>
+                            </div>
+                            <div className="col-6 col-md-3">
+                                <p className="mb-0 spell-key">Reaction Condition</p>
+                                <p><strong>{props.reaction_condition}</strong></p>
                             </div>
                             <div className="col-6 col-md-3">
                                 <p className="mb-0 spell-key">Range</p>
@@ -32,7 +41,26 @@ export default function Spells(props) {
                                 <p className="mb-0 spell-key">Ritual</p>
                                 {props.ritual && <p><strong>True</strong></p>}
                                 {!props.ritual && <p><strong>False</strong></p>}
-
+                            </div>
+                            <div className="col-6 col-md-4">
+                                <p className="mb-0 spell-key">Somatic</p>
+                                {props.component_somatic && <p><strong>True</strong></p>}
+                                {!props.component_somatic && <p><strong>False</strong></p>}
+                            </div>
+                            <div className="col-6 col-md-4">
+                                <p className="mb-0 spell-key">Verbal</p>
+                                {props.component_verbal && <p><strong>True</strong></p>}
+                                {!props.component_verbal && <p><strong>False</strong></p>}
+                            </div>
+                            <div className="col-6 col-md-4">
+                                <p className="mb-0 spell-key">Material</p>
+                                {props.component_material && <p><strong>True</strong></p>}
+                                {!props.component_material && <p><strong>False</strong></p>}
+                            </div>
+                            <div className="col-6 col-md-4">
+                                <p className="mb-0 spell-key">Material Description</p>
+                                {props.material_description && <p><strong>{props.material_description}</strong></p>}
+                                {/* {!props.material_description && <p><strong>False</strong></p>} */}
                             </div>
                             <div className="col-6 col-md-4">
                                 <p className="mb-0 spell-key">School</p>
@@ -45,6 +73,8 @@ export default function Spells(props) {
                             <div className="col-12">
                                 <p className="mb-0 spell-key">Description</p>
                                 <p><strong>{props.description}</strong></p>
+                                <p className="mb-0 spell-key">Higher Level</p>
+                                <p><strong>{props.higher_levels}</strong></p>
                             </div>
                         </div>
                     </div>
@@ -59,16 +89,16 @@ export default function Spells(props) {
                             </div>
                             <div className="col-6 col-md-2 order-1 order-md-2">
                                 <p className="mb-0 spell-key">classes</p>
-                                {/* <p><strong>{props.classes.map(x => x + " ")}</strong></p> */}
+                                <p><strong>{props.classes}</strong></p>
                             </div>
-                            <div className="col-6 order-4 order-md-3">
-                                <p className="mb-0 spell-key">Components</p>
-                                <p><strong>{props.components}</strong></p>
-                            </div>
-                            <div className="col-6 col-md-2 order-2 order-md-4">
-                                <p className="mb-0 spell-key">tags</p>
+                            {/* <div className="col-6 order-4 order-md-3"> */}
+                                {/* <p className="mb-0 spell-key">Components</p> */}
+                                {/* <p><strong>{props.components}</strong></p> */}
+                            {/* </div> */}
+                            {/* <div className="col-6 col-md-2 order-2 order-md-4"> */}
+                                {/* <p className="mb-0 spell-key">tags</p> */}
                                 {/* <p><strong>{props.tags.map(x => x + " ")}</strong></p> */}
-                            </div>
+                            {/* </div> */}
                         </div>
                     </div>
                 </div>
