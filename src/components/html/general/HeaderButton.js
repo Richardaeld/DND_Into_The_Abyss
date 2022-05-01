25a57da96bb2a0cb1bebd69653ce565573f33073
Richardaeld <Richardaeld@gmail.com>
2022-05-01 02:24:53 -0400

    Corrected a button size bug

--- src/components/html/general/HeaderButton.js
+++ src/components/html/general/HeaderButton.js
@@ -10,7 +10,7 @@ export default function NavButton(props) {
     return (
         <>
             {!props.open && <div
-                className="nav-button col-6 col-md-3 flex-content-center py-2 nonselectable"
+                className="nav-button col-6 col-md-3 flex-content-center py-2 nonselectable header-standard"
                 style={styles}
                 onClick={props.toggleNav}
             >
@@ -18,7 +18,7 @@ export default function NavButton(props) {
             </div>}
 
             {props.open && <div
-                className="nav-button deep-shadow col-6 col-md-3 flex-content-center py-2 nonselectable"
+                className="nav-button deep-shadow col-6 col-md-3 flex-content-center py-2 nonselectable header-standard"
                 style={styles}
                 onClick={props.toggleNav}
             >
