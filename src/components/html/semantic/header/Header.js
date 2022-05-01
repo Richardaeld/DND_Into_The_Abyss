b20dc59a4b29e16f947692437c77d4e6d33aad18
Richardaeld <Richardaeld@gmail.com>
2022-05-01 01:35:13 -0400

    Corrected a nonhighlight bug and beautified code

--- src/components/html/semantic/header/Header.js
+++ src/components/html/semantic/header/Header.js
@@ -4,7 +4,6 @@ import SubHeaderButton from '../../general/SubHeaderButton'
 // import React from "react";
 export default function HeaderNavButtons(props) {
 
-
     const [navNames, setNavNames] = React.useState([])
 
     // Sets Header (Main) Nav buttons
@@ -28,62 +27,66 @@ export default function HeaderNavButtons(props) {
     // Toggle visibility of sub nav buttons
     function toggleNav(id, targetId) {
 
+        // Reveal nav button content
+        setNavNames(prevNavNames => {
+            // remove any existing printout content
+            props.setOutTarget(["", "", ""])
+            props.setSpellInfo([])
 
-    // Reveal nav button content
-    setNavNames(prevNavNames => {
-        // remove any existing printout content
-        props.setOutTarget(["", "", ""])
-        props.setSpellInfo([])
+            return prevNavNames.map((navName) => {
+                if (navName.id === id) {
 
-        return prevNavNames.map((navName) => {
-            console.log(navName.name, "test")
-            if (navName.id === id) {
-            // Fetch Sub Header Buttons
-            const getSubNav = async () => {
-                try{
-                console.log(navName.id, "DBhit")
+                    // Fetch Sub Header Buttons
+                    (async () => {
+                        try{
+                        // console.log(navName.id, "DB hit")
 
-                const response =await fetch(`https://dnd-rolling-chart-api.herokuapp.com/api/button/main/viewAll/children/${navName.id}`)
-                const jsonData = await response.json()
+                        const response =await fetch(`https://dnd-rolling-chart-api.herokuapp.com/api/button/main/viewAll/children/${navName.id}`)
+                        const jsonData = await response.json()
 
-                props.setSubNavNames(jsonData['button'])
+                        props.setSubNavNames(jsonData['button'])
 
-                console.log(jsonData['button'], "IM SUB HEADER JSON DATA")
-                } catch (err) {
-                console.error(err)
+                        console.log(jsonData['button'], "IM SUB HEADER JSON DATA")
+                        } catch (err) {
+                        console.error(err)
+                        }
+                    })();
+
+                    // Ensures only a off click will remove highlight
+                    if (navName.open) {
+                        return {...navName}
+                    } else {
+                        return {...navName, open: !navName.open}
+                    }
+
+                } else {
+                    return {...navName, open: false}
                 }
-            }
-                getSubNav();
-            return {...navName, open: !navName.open}
-            } else {
-            return {...navName, open: false}
-            }
-
-
-        })
+            })
         })
     }
-  const navElements = navNames.map(navName => (
-    <HeaderButton
-        key={navName.id}
-        name={navName.name}
-        open={navName.open}
-        target={navName.target}
-        toggleNav={() => toggleNav(navName.id, navName.target)}
-    />
-  ))
 
-  const subNavElements = props.subNavNames.map(button => (
-    <SubHeaderButton
-        key={button.name}
-        name={button.name}
-        obj_name={button.obj_name}
-        child_table={button.child_table}
-        open={button.true}
-        click={() => props.toggleNewNav(button.id, button.name, button.obj_name, button.random_roll_only)}
-        clickSpells={() => props.toggleSpellNav(button.id, button.name, button.level)}
-    />
-  ))
+    const navElements = navNames.map(navName => (
+        <HeaderButton
+            key={navName.id}
+            name={navName.name}
+            open={navName.open}
+            target={navName.target}
+            toggleNav={() => toggleNav(navName.id, navName.target)}
+        />
+    ))
+
+    const subNavElements = props.subNavNames.map(button => (
+        <SubHeaderButton
+            key={button.name}
+            name={button.name}
+            obj_name={button.obj_name}
+            child_table={button.child_table}
+            open={button.true}
+            click={() => props.toggleNewNav(button.id, button.name, button.obj_name, button.random_roll_only)}
+            clickSpells={() => props.toggleSpellNav(button.id, button.name, button.level)}
+        />
+    ))
 
     return (
         <header className="container-fluid" >
@@ -101,7 +104,5 @@ export default function HeaderNavButtons(props) {
                 </div>
             }
       </header>
-
-
-        )
+    )
 }
