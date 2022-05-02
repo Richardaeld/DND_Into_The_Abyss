b28ace77c3e51d2a114cf62702fd62d56cd578f3
Richardaeld <Richardaeld@gmail.com>
2022-05-01 22:53:00 -0400

    Updated root to react 18 version

--- src/index.js
+++ src/index.js
@@ -1,16 +1,25 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 // import reportWebVitals from './reportWebVitals';
 // import NavButton from "./components/NavButton"
 
-ReactDOM.render(
+const root = document.getElementById('root')
+const ReactRoot = ReactDOM.createRoot(root)
+
+ReactRoot.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  </React.StrictMode>
+)
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
